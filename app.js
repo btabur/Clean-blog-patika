e5b5c5597adc040e7da5b029ab9f1bfe707fefd7
Balı Tabur <webtasarim.0323@gmail.com>
2024-01-30 13:06:04 +0300

    uptaded workspace

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
 
-const blog = {
-    id:1,
-    title:'Blog title',
-    description:'Blog description'
-}
-app.get('/', (req, res)=> {
-  res.send(blog)
-})
+//template engine
+app.set('view engine', 'ejs');
 
-app.listen(3000,()=> {
-    console.log('Sunucu Çalıştı')
-})
+//middlewares
+app.use(express.static('public'));
+
+// const blog = {
+//   id: 1,
+//   title: 'Blog title',
+//   description: 'Blog description',
+// };
+// app.get('/', (req, res) => {
+//   res.send(blog);
+// });
+
+
+//routes
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+
+  res.render('about')
+});
+
+app.get('/add', (req, res) => {
+
+  res.render('add_post')
+});
+
+app.get('/post', (req, res) => {
+
+  res.render('post')
+});
+
+app.listen(3000, () => {
+  console.log('Sunucu Çalıştı');
+});
