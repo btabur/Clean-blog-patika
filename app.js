293754c54997f581814dd2b9ffcc4a9574382503
Balı Tabur <webtasarim.0323@gmail.com>
2024-02-01 14:38:05 +0300

    Code editing was done in accordance with MVC architecture.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const ejs = require('ejs')
+const ejs = require('ejs');
 const path = require('path');
 const Post = require('./models/Post');
 const methodOverride = require('method-override');
+const postController = require('./controllers/postController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -11,13 +13,11 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/clean-blog-test-db');
 
 //template engine
-app.set("view engine","ejs")
-
+app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.static('public'));
-
-app.use(express.urlencoded({extended :true}))  
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   methodOverride('_method', {
@@ -25,61 +25,22 @@ app.use(
   })
 );
 
-
 //routes
-app.get('/', async(req, res) => {
-  const posts = await Post.find({})
-res.render('index',{
-  posts
-})
-});
+app.get('/', postController.getAllPost);
 
-app.get('/post/:id', async(req, res) => {
- 
- const post = await Post.findById(req.params.id)
+app.get('/post/:id', postController.getPost);
 
- res.render('post',{
-  post
- })
-});
+app.get('/about', pageController.getAboutPage);
 
-app.get('/about', (req, res) => {
+app.get('/add', pageController.getAddPage);
 
-  res.render('about')
-});
+app.post('/post', postController.createPost);
 
-app.get('/add', (req, res) => {
+app.get('/post/edit/:id', pageController.getEditPage);
 
-  res.render('add_post')
-});
+app.put('/post/:id', postController.updatePost);
 
-app.post('/post', async(req, res) => {
-
-  await Post.create(req.body)
-  res.redirect('/')
-});
-
-app.get('/post/edit/:id', async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
-  res.render('edit', {
-    post,
-  });
-});
-
-app.put('/post/:id', async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
-  post.title = req.body.title
-  post.detail = req.body.detail
-  post.save()
-
-  res.redirect(`/post/${req.params.id}`)
-});
-
-app.delete('/post/:id', async (req, res) => {
-  await Post.findByIdAndDelete(req.params.id);
-  res.redirect('/');
-});
-
+app.delete('/post/:id', postController.deletePost);
 
 app.listen(3000, () => {
   console.log('Sunucu Çalıştı');
